14fff39df5aa44b9247873c24f99321bc0a01387
maan <pranijman@gmail.com>
2012-12-17 13:30:17 +0545

    Reverting to <Merge branch'develop'>

---
+++ application/migrations/018_add_employments.php
@@ -0,0 +1,41 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_employments extends CI_Migration {
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->dbforge();
+	}
+
+
+	public function up()
+	{
+		$fields = array(
+						    '`id` int(11) NOT NULL',
+                '`title` varchar(127) CHARACTER SET utf8 NOT NULL',
+                '`content` varchar(255) CHARACTER SET utf8 NOT NULL',
+                '`usefullinks_type` int(127) NOT NULL',
+                '`created_by` int(11) NOT NULL',
+                '`date_created` timestamp NULL DEFAULT NULL',
+                '`date_published` timestamp NULL DEFAULT NULL',
+                '`date_removed` timestamp NULL DEFAULT NULL',
+                '`active` tinyint(4) NOT NULL',
+                '`homepage` tinyint(4) NOT NULL'
+
+					);
+		$this->dbforge->add_field($fields);
+
+		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->create_table('employments');
+		//--------------------------------------------
+
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_table('employments');
+	}
+}
+
+/* End of file 007_add_employments.php */
+/* Location: ./application/migrations/008_add_employments.php */
