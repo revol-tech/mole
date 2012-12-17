72126bc0a503668c1593d8b27a18bc16a154ffb1
dhami <rdhami425@gmail.com>
2012-12-17 11:56:03 +0545

    update migrations tables

--- application/migrations/017_add_networks.php
+++ application/migrations/017_add_networks.php
@@ -11,16 +11,16 @@ class Migration_Add_networks extends CI_Migration {
 	public function up()
 	{
 		$fields = array(
-						    `id` int(11) NOT NULL,
-                `title` varchar(127) CHARACTER SET utf8 NOT NULL,
-                `content` varchar(255) CHARACTER SET utf8 NOT NULL,
-                `networks_type` int(127) NOT NULL,
-                `created_by` int(11) NOT NULL,
-                `date_created` timestamp NULL DEFAULT NULL,
-                `date_published` timestamp NULL DEFAULT NULL,
-                `date_removed` timestamp NULL DEFAULT NULL,
-                `active` tinyint(4) NOT NULL,
-                `homepage` tinyint(4) NOT NULL
+						    '`id` int(11) NOT NULL',
+                '`title` varchar(127) CHARACTER SET utf8 NOT NULL',
+                '`content` varchar(255) CHARACTER SET utf8 NOT NULL',
+                '`usefullinks_type` int(127) NOT NULL',
+                '`created_by` int(11) NOT NULL',
+                '`date_created` timestamp NULL DEFAULT NULL',
+                '`date_published` timestamp NULL DEFAULT NULL',
+                '`date_removed` timestamp NULL DEFAULT NULL',
+                '`active` tinyint(4) NOT NULL',
+                '`homepage` tinyint(4) NOT NULL'
 
 					);
 		$this->dbforge->add_field($fields);
--- application/migrations/019_add_vip.php
+++ application/migrations/019_add_vip.php
@@ -11,15 +11,15 @@ class Migration_Add_vip extends CI_Migration {
 	public function up()
 	{
 		$fields = array(
-						  `id` int(11) NOT NULL,
-              `filename` varchar(127) CHARACTER SET utf8 NOT NULL,
-              `title` varchar(127) CHARACTER SET utf8 NOT NULL,
-              `description` varchar(255) CHARACTER SET utf8 NOT NULL,
-              `timestamp` varchar(127) CHARACTER SET utf8 NOT NULL,
-              `created_by` int(100) NOT NULL,
-              `date_created` timestamp NULL DEFAULT NULL,
-              `date_published` timestamp NULL DEFAULT NULL,
-              `file_type` varchar(127) CHARACTER SET utf8 NOT NULL
+						  '`id` int(11) NOT NULL',
+              '`filename` varchar(127) CHARACTER SET utf8 NOT NULL',
+              '`title` varchar(127) CHARACTER SET utf8 NOT NULL',
+              '`description` varchar(255) CHARACTER SET utf8 NOT NULL',
+              '`timestamp` varchar(127) CHARACTER SET utf8 NOT NULL',
+              '`created_by` int(100) NOT NULL',
+              '`date_created` timestamp NULL DEFAULT NULL',
+              '`date_published` timestamp NULL DEFAULT NULL',
+              '`file_type` varchar(127) CHARACTER SET utf8 NOT NULL'
 					);
 		$this->dbforge->add_field($fields);
 
